736b9c6d846f5a4c079c2a37e75f2289a6f8e7e9
carlospepato <71358695+carlospepato@users.noreply.github.com>
2024-07-26 15:57:36 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+interface Pessoa{
+  nome: string;
+  idade: number;
+}
+
+const pessoa: Pessoa = {
+  nome: 'João',
+  idade: 20
+};
+
+console.log(pessoa);
---
+++ frontend/src/app.tsx
@@ -0,0 +1,5 @@
+export function App() {
+  return (
+    <h1 className="underline">hello world</h1>
+  )
+}
